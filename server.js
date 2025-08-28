f50c18fc636c52c061e960741abea8810d5495a2
Yash Raj <kyashraj991@gmail.com>
2025-08-28 23:50:55 +0530

    refactor: standardize quotes and improve socket user management logic

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-import { createServer } from "node:http";
-import next from "next";
-import { Server } from "socket.io";
+import { createServer } from 'node:http';
+import next from 'next';
+import { Server } from 'socket.io';
 
-const dev = process.env.NODE_ENV !== "production";
-const hostname = "localhost";
+const dev = process.env.NODE_ENV !== 'production';
+const hostname = 'localhost';
 const port = 3000;
 // when using middleware `hostname` and `port` must be provided below
 const app = next({ dev, hostname, port });
@@ -14,12 +14,35 @@ app.prepare().then(() => {
 
   const io = new Server(httpServer);
 
-  io.on("connection", (socket) => {
+  let onlineUsers = [];
+
+  io.on('connection', (socket) => {
+    console.log('Client connected:', socket.id);
+
+    socket.on('addNewUser', (authUser) => {
+      if (
+        authUser &&
+        !onlineUsers.some((user) => user?.userId === authUser.id)
+      ) {
+        onlineUsers.push({
+          userId: authUser.id,
+          sockedId: socket.id,
+          profile: authUser,
+        });
+        io.emit('getUser', onlineUsers);
+      }
+    });
+
+    socket.on('disconnect', () => {
+      onlineUsers = onlineUsers.filter((user) => user.sockedId !== socket.id);
+
+      io.emit('getUsers', onlineUsers);
+    });
     // ...
   });
 
   httpServer
-    .once("error", (err) => {
+    .once('error', (err) => {
       console.error(err);
       process.exit(1);
     })
@@ -28,4 +51,4 @@ app.prepare().then(() => {
     });
 });
 
-console.log("Server Started::30000")
+console.log('Server Started::30000');
